b55bc7f66cb8f8fa263b96aaac77374f178406eb
mis <sirigiriv@unicourt.com>
2023-10-04 20:34:15 +0530

    Added Lint Report

--- tests/Authentication/Users/users.test.ts
+++ tests/Authentication/Users/users.test.ts
@@ -1,13 +1,19 @@
 import * as  data from '../Users/data.json';
-import { test} from "@playwright/test";
+import { test, expect} from "@playwright/test";
 test('Test Page', async ({ page   }) => {
 
 await page.goto('https://testpages.herokuapp.com/styled/tag/dynamic-table.html');
-let dataToBeEntered = JSON.stringify(data.default);
+const dataToBeEntered = JSON.stringify(data.default);
  await page.locator('text=Table Data').click();
  const jsonDataLocator = page.locator('id=jsondata');
  await jsonDataLocator.fill(dataToBeEntered);
  await page.waitForTimeout(2000);
  await page.getByRole('button', { name: 'Refresh table' } ).click();
  await page.waitForTimeout(2000);  
+
+// if we comment below lines and generate lint report we will get warnings saying test has Test has no assertions, if we uncomment we may see 0 errors
+//  const columnName  = page.locator("//th[text()='name']");
+//  const columnNameValue  = await columnName.textContent();
+// await expect(columnNameValue).toContain('name');
+
 });
